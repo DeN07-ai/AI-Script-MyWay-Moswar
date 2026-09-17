6d5ecd081368eb1d26df4037daa62bbc753a8092
DeN07-ai <denisgrishko07@gmail.com>
2026-09-17 20:22:46 +0300

    Update AI MyWay DeN.js
    multi open case ulushator mod

--- AI MyWay DeN.js
+++ AI MyWay DeN.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AI MyWay DeN
 // @namespace    MyWay.Moswar
-// @version      2.5.9
+// @version      3.0
 // @author       MyWay DeN
 // @description  Модульный скрипт для moswar.ru: рейды, крысы, нефть, подземка, флаг, спутники, ИИ, Фулл Доп, закупка ТЦ, Фу-Баги, ОМОН, Око Провидения
 // @match        https://*.moswar.ru/*
